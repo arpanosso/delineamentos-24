5c22556d234848fdd3e86f721f384ac690450af6
arpanosso <arpanosso@yahoo.com.br>
2024-08-21 14:05:14 -0300

    aula 02 - 1/2

---
+++ data-raw/faxina-de-dados.R
@@ -0,0 +1,42 @@
+# Faxina dos dados da aulas 02
+library(tidyverse)
+library(readxl)
+
+# Entrada de Dados
+data_set <- read_xlsx("data-raw/aula2.xlsx")
+
+# Mostrando o objeto
+data_set
+
+# Média de RESP
+data_set %>%
+  pull(RESP) %>%
+  mean()
+
+data_set %>%
+  mutate(RESP2 = RESP^2)
+
+# Arrumando os nomes das colunas
+data_set <- data_set %>%
+  janitor::clean_names()
+data_set
+
+# Visualização
+data_set %>%
+  ggplot(aes(y= resp)) +
+  geom_boxplot(fill="orange")+
+  xlim(-1,1)
+
+data_set %>%
+  ggplot(aes(y= resp, x=trat, fill = trat)) +
+  geom_boxplot() +
+  theme_bw() +
+  scale_fill_viridis_d() +
+  labs(fill="",y="Produção da Cana (TCH)")
+
+
+
+
+
+
+
